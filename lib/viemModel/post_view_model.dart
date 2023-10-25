94708a40efdf8c09bf522831444762bbfdba0df2
Hachemi Ibrahim <123593839+HachemiIbrahim@users.noreply.github.com>
2023-10-25 17:23:02 +0100

    adding post viewModel

---
+++ lib/viemModel/post_view_model.dart
@@ -0,0 +1,3 @@
+class PostViewModel {
+  final title = "Posts";
+}
--- lib/view/post_screen.dart
+++ lib/view/post_screen.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:mvvm_api_t/viemModel/post_view_model.dart';
 
+// ignore: must_be_immutable
 class PostScreen extends StatelessWidget {
-  const PostScreen({Key? key}) : super(key: key);
-
+  PostScreen({Key? key}) : super(key: key);
+  var postViewModel = PostViewModel();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Post Screen'),
+        title: Text(postViewModel.title),
       ),
       body: const Center(
         child: Text('Post Screen'),
